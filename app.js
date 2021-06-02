807a437ca82f4964f0245dee4392b370e0b9e37d
wuchangwei <985215993@qq.com>
2021-06-02 20:15:15 +0800

    学页面级别的生命周期函数

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-// app.js
+// app.js 创建一个小程序
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+    // 生命中期函数
+    // 在小程序启动的时候，自动执行的函数
+    // scene场景指的是从哪个入口进来的
+    onLaunch(options){
+      console.log('onLaunch',options);
+      this.sayHello()
+    },
+    // 在小程序重新展示的时候执行的函数
+    onShow(options){
+      console.log('onShow',options);
+    },
+    // 在小程序取消展示的时候执行的函数无参数
+    onHide(){
+      console.log('onHide');
+    },
+    //当脚本执行错误时自动执行的函数
+    onError(msg){
+      console.log('onError',msg);
+    },
+    sayHello(){
+      return 'say Hello'
+    },
+    globalData: {
+    }
 })
