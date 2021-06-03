ee1f99c178c9a34f34b2ab9d40845ee2e25af909
wuchangwei <985215993@qq.com>
2021-06-03 15:32:33 +0800

    学习小程序基础结束 初始化小程序项目

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
 // app.js 创建一个小程序
 App({
-    // 生命中期函数
-    // 在小程序启动的时候，自动执行的函数
-    // scene场景指的是从哪个入口进来的
-    onLaunch(options){
-      console.log('onLaunch',options);
-      this.sayHello()
-    },
-    // 在小程序重新展示的时候执行的函数
-    onShow(options){
-      console.log('onShow',options);
-    },
-    // 在小程序取消展示的时候执行的函数无参数
-    onHide(){
-      console.log('onHide');
-    },
-    //当脚本执行错误时自动执行的函数
-    onError(msg){
-      console.log('onError',msg);
-    },
-    sayHello(){
-      return 'say Hello'
-    },
-    globalData: {
-    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,5 @@
 // index.js
 // 获取应用实例
-const app = getApp()
 Page({
-  onLoad(){
-    console.log(app.sayHello(),app.globalData,'index onLoad');
-  }
+  
 })
