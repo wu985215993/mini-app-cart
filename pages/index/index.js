670bd415e58050525247a4ffa625daccb6bcc1a9
wuchangwei <985215993@qq.com>
2021-06-03 17:12:33 +0800

    首页搜索栏样式与输入事件隐藏placeholder和icon完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,13 @@
 // index.js
 // 获取应用实例
 Page({
-  
+  data: {
+    showIcon: true
+  },
+  handleInputChange(e) {
+    const value = e.detail.value;
+    this.setData({
+      showIcon: value ? false : true
+    })
+  }
 })
